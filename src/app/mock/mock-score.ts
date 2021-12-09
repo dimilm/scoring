94863b7f0364341b7a64659c2f75221fa800dc99
Dimi <dimitri@uragan.de>
2021-12-09 23:34:18 +0100

    scores changed

--- src/app/mock/mock-score.ts
+++ src/app/mock/mock-score.ts
@@ -2,8 +2,7 @@ import { GameRound } from "../model/game-round";
 import { ScoreMember } from "../model/score-member";
 
 export const SCORE_MEMBERS: ScoreMember[] = [
-  { id: 11, name: 'Opa', score:0 },
-  { id: 12, name: 'Oma', score: 0 },
+  { id: 11, name: 'Opa & Oma', score:0 },
   { id: 20, name: 'Tante Katja', score: 0 },
   { id: 13, name: 'Fedja', score: -1 },
   { id: 14, name: 'Katja', score: 1 },
